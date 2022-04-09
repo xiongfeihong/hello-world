6bbe064b33ac0722524ad5f6d68fcc70d0bf1bed
xiongfeihong <63570554+xiongfeihong@users.noreply.github.com>
2022-04-09 18:33:20 +0800

    Create echarts_index.vue

---
+++ echarts_index.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+    <PieCharts :itemWidth="20" />
+    <PieCharts3/>
+</div>
+</template>
+<script>
+import PieCharts from './pieCharts.vue'
+import PieCharts3 from './pieCharts3.vue'
+export default {
+    components:{
+        PieCharts,
+        PieCharts3
+    },
+    data() {
+        return {
+            obj:{
+                
+            }
+        }
+    },
+    created(){
+
+    }
+};
+</script>
+<style>
+</style>
