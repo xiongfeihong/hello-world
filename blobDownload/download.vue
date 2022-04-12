53dd078fd1d2f6c5707dde174dfdb225bef14f98
xiongfeihong <63570554+xiongfeihong@users.noreply.github.com>
2022-04-12 20:59:25 +0800

    Create download.vue

---
+++ blobDownload/download.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-button size="small" type="primary" plain @click="exportData()">导出</el-button>
+</template>
+
+<script>
+import {
+  exportBlob
+} from '@/util/export.js'
+import statisBoard from "@/apis/statisBoard.js";
+
+export default {
+  data() {
+    return {}
+  },
+  methods: {
+    exportData() {
+      let parm = this.getParams();
+      statisBoard.exportPersonTotList(parm).then(res => {
+        if (res) {
+          // 导出csv
+          exportBlob(res);
+        }
+      })
+    }    
+  }
+}
+
+</script>
+
